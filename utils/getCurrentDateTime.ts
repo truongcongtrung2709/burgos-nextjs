f6f6c1d6c525c8ccefdb7d023c37c291e911402f
truongcongtrung <truongcongtrung2709@gmail.com>
2023-11-22 13:03:36 +0700

    fix time

--- utils/getCurrentDateTime.ts
+++ utils/getCurrentDateTime.ts
@@ -1,8 +1,10 @@
 export const getCurrentDateTime = () =>{
   const today = new Date();
-  const time = today.getTime();
+  const hour = today.getHours();
+  const minutes = today.getMinutes();
+  const seconds = today.getSeconds();
   const month = today.getMonth() + 1;
   const year = today.getFullYear();
   const date = today.getDate();
-  return `${time}/${month}/${date}/${year}`;
+  return `${hour}:${minutes}:${seconds}/${month}/${date}/${year}`;
 }
